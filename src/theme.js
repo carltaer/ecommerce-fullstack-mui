0bd206d71acb473a97753ff9207a557ef4967481
carltaer <carlchristopher.taer@gmail.com>
2023-01-22 15:47:28 +0000

    first commit

---
+++ src/theme.js
@@ -0,0 +1,29 @@
+import { useTheme } from "@material-ui/core/styles"
+
+const theme = useTheme ({
+    palette: {
+        primary: {
+            main: "#fff",
+        },
+        secondary: {
+            main: "#333",
+        },
+    },
+    typography: {
+        h1: {
+            fontFamily: "Nova Slim",
+            fontSize: 36,
+        },
+        fontFamily: "Montserrat",
+        subtitle1: {
+            lineHeight: 1.5,
+            paddingLeft: 10,
+            opacity: 0.8,
+        },
+        subtitle2: {
+            fontWeight: 600,
+        },
+    },
+})
+
+export default th
